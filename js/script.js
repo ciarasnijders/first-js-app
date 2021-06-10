03b820db2f23e3082cca9fac187546a2eeea0ae1
Ciara <ciarazsnijders@gmail.com>
2021-06-10 20:28:46 +0200

    Added the key: types , to the variable pokemonList

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 //This array contains Pokémon data to display in the application.
-var pokemonList = [
+let pokemonList = [
     {
         name:"Pikachu",
         height: 5,
         colors: ["yellow", "black", "red"],
+        type: ["electric"],
     },
     {
         name:"Bulbasaur",
         height: 7,
         color: ["blue", "green", "turquoise"],
+        type: ["grass", "poison"],
     },
     {
         name:"Charmander",
         height: 10,
         color: ["orange", "yellow", "red"],
+        type: ["fire"],
     }
 ]; 
