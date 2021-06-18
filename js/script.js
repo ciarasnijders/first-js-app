52f4b3e48bff81ea554a7de861976b15b027b917
Ciara <ciarazsnijders@gmail.com>
2021-06-18 17:22:07 +0200

    wrapped the pokemonList array in an IIFE, created getAll() and add()

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
-//This array contains Pokémon data to display in the application.
-let pokemonList = [
-    {
-        name:"Pikachu",
-        height: 4,
-        colors: ["yellow", "black", "red"],
-        type: ["electric"],
-    },
-    {
-        name:"Bulbasaur",
-        height: 7,
-        color: ["blue", "green", "turquoise"],
-        type: ["grass", "poison"],
-    },
-    {
-        name:"Charmander",
-        height: 6,
-        color: ["orange", "yellow", "red"],
-        type: ["fire"],
-    }
-];
+let pokemonRepository = (function() {
+    //This array contains Pokémon data to display in the application.
+    let pokemonList = [
+        {
+            name:"Pikachu",
+            height: 4,
+            colors: ["yellow", "black", "red"],
+            type: ["electric"],
+        },
+        {
+            name:"Bulbasaur",
+            height: 7,
+            color: ["blue", "green", "turquoise"],
+            type: ["grass", "poison"],
+        },
+        {
+            name:"Charmander",
+            height: 6,
+            color: ["orange", "yellow", "red"],
+            type: ["fire"],
+        }
+    ];
 
-//a forEach loop that iterates over each item in pokemonList
+    function getAll(){
+        return pokemonList;
+    }
+
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+}) ();
+
+//a forEach() loop that iterates over each item in pokemonList
 function pokemonLoopFunction(pokemon) {
-    let partial = `${pokemonList[i].name} (height: ${pokemonList[i].height}) `;
+    let partial = `${pokemon.name} (height: ${pokemon.height}) `;
+
     if (pokemon.height >= 7){
         document.write(`<p class="pokemon-list__item special">${partial} - Wow, that’s big! </p>`);
     } else {
         document.write(`<p class="pokemon-list__item special">${partial} </p>`);
     }
-  }
-  pokemonList.forEach(pokemonLoopFunction);
+    }
+
+    pokemonRepository.getAll().forEach(pokemonLoopFunction);
